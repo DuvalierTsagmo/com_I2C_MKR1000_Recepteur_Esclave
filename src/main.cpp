a366c02ed9fd10301ccbd6268820b3d94cfb09f3
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-02-15 09:47:31 -0400

    le uC qui recois qui est le mkr1000

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
+/*
+  Titre      : communication entre 2 uC MKR1000 qui est l'esclave
+  Auteur     : Duvalier Tsagmo
+  Date       : 15/02/2022
+  Description: la communication est I2C QUI va se faire entre deux uC
+  Version    : 0.0.1
+*/
 
 #include <Arduino.h>
 #include <Wire.h>
 
+// Declaration des variables
 const int LED_PIN = 0;
 int Recoi_Donnee = 0;
 int Allume = 0;
 
+// prototype de fonctionnement pour celui qui recoi
 void Recevoir(int b)
 {
     Recoi_Donnee = Wire.read();
